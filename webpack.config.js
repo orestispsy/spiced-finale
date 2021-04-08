c79457f4b13dd8664811047d5100ec8ea88d34ee
Orestis Psycharis <opsychar@gmail.com>
2021-04-08 11:24:35 +0200

    heroku testing

--- webpack.config.js
+++ webpack.config.js
@@ -54,8 +54,9 @@ module.exports = () => ({
         new MiniCssExtractPlugin({
             filename: "bundle.css",
         }),
-        new webpack.ProvidePlugin({
-            process: "process/browser",
+        new webpack.DefinePlugin({
+            NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+            API_HOST: JSON.stringify(process.env.API_HOST),
         }),
     ],
 });
