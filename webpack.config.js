994fd210f05421b36d389baf74b5a4760396c7ee
Orestis Psycharis <opsychar@gmail.com>
2021-04-08 11:06:15 +0200

    heroku testing

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,8 @@ module.exports = () => ({
             {
                 test: /\.js$/,
                 loader: "babel-loader",
-            }, {
+            },
+            {
                 test: /\.css$/i,
                 use: [
                     MiniCssExtractPlugin.loader,
@@ -49,15 +50,12 @@ module.exports = () => ({
             },
         ],
     },
-    plugins:  [new MiniCssExtractPlugin({
-        filename: 'bundle.css',
-    }), new webpack.DefinePlugin({
-  PRODUCTION: JSON.stringify(true),
-  VERSION: JSON.stringify('5fa3b9'),
-  BROWSER_SUPPORTS_HTML5: true,
-  TWO: '1+1',
-  'typeof window': JSON.stringify('object'),
-  'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-})],
-    
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "bundle.css",
+        }),
+        new webpack.ProvidePlugin({
+            process: "process/browser",
+        }),
+    ],
 });
