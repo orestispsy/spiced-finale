84f7d3848527694553b4d31fc578f0a429e47ea9
Orestis Psycharis <opsychar@gmail.com>
2021-04-30 14:44:24 +0200

    added an intro revealing slider!

--- client/src/app.js
+++ client/src/app.js
@@ -9,6 +9,8 @@ import GigEditor from "./gigEditor";
 import GigList from "./gigList";
 import Chat from "./chat";
 
+var body = document.querySelectorAll("body");
+
 export default class App extends Component {
     constructor(props) {
         super(props);
@@ -17,7 +19,10 @@ export default class App extends Component {
             list: false,
             visitors: false,
             chat_img: false,
-            chat_color: false
+            chat_color: false,
+            left: body[0].offsetWidth,
+            sliderWidth: 0,
+            move: false,
         };
     }
 
@@ -32,7 +37,7 @@ export default class App extends Component {
                         nickname: data.data.nickname,
                         admin: data.data.admin,
                         chat_img: data.data.chat_img,
-                        chat_color: data.data.chat_color
+                        chat_color: data.data.chat_color,
                     });
                 }
             })
@@ -75,6 +80,35 @@ export default class App extends Component {
         });
     }
 
+    sliderAction(e) {
+        e.preventDefault();
+        this.setState({
+            move: true,
+            sliderWidth: e.target.clientWidth,
+        });
+    }
+
+    sliderStop() {
+        this.setState({
+            move: false,
+        });
+    }
+
+    handleMouseMove(e) {
+        if (e.clientX >= body[0].offsetWidth - this.state.sliderWidth / 8) {
+            this.setState({
+                move: false,
+                left: body[0].offsetWidth,
+            });
+        }
+
+        if (this.state.move) {
+            this.setState({
+                left: e.clientX,
+            });
+        }
+    }
+
     logOut() {
         axios
             .get("/logout")
@@ -93,94 +127,134 @@ export default class App extends Component {
         return (
             <BrowserRouter>
                 <div
-                    className={
-                        (this.state.list && "appContainerList") ||
-                        (!this.state.maps && "appContainer") ||
-                        (this.state.maps && "appContainerMap")
-                    }
+                    onMouseMove={(e) => this.handleMouseMove(e)}
+                    onMouseUp={() => this.sliderStop()}
                 >
-                    <div className="appBar">
-                        <div className="barLeftSection">
-                            <div className="barProfile">
-                                {this.state.nickname}
+                    <div className="introPreCover">
+                        <div
+                            className={
+                                (this.state.list && "appContainerList") ||
+                                (!this.state.maps && "appContainer") ||
+                                (this.state.maps && "appContainerMap")
+                            }
+                        >
+                            <div className="appBar">
+                                <div className="barLeftSection">
+                                    <div className="barProfile">
+                                        {this.state.nickname}
+                                    </div>
+                                    {!this.state.maps && (
+                                        <Link to="/chat">
+                                            <div className="chatBar"></div>
+                                        </Link>
+                                    )}
+                                </div>
+                                <a
+                                    target="_blank"
+                                    href="https://www.1000mods.com"
+                                >
+                                    <div className="logo2Back">
+                                        <div className="logo2"></div>
+                                    </div>
+                                </a>
+                                <div
+                                    className="logout"
+                                    onClick={() => this.logOut()}
+                                >
+                                    LogOut
+                                </div>
+                                {this.state.maps && (
+                                    <Link
+                                        to="/"
+                                        className="barMainLink"
+                                        onClick={() => this.mapVisible()}
+                                    ></Link>
+                                )}
                             </div>
-                            {!this.state.maps && (
-                                <Link to="/chat">
-                                    <div className="chatBar"></div>
-                                </Link>
-                            )}
+                            <Route
+                                exact
+                                path="/"
+                                render={(props) => (
+                                    <Main
+                                        admin={this.state.admin}
+                                        listSet={(e) => this.listSet(e)}
+                                        visitors={this.state.visitors}
+                                    />
+                                )}
+                            />
+                            <Route
+                                exact
+                                path="/gig-creator"
+                                render={(props) => (
+                                    <GigCreator admin={this.state.admin} />
+                                )}
+                            />
+                            <Route
+                                exact
+                                path="/gig-editor"
+                                render={(props) => (
+                                    <GigEditor gigsList={this.state.gigsList} />
+                                )}
+                            />
+                            <Route
+                                exact
+                                path="/map"
+                                render={(props) => (
+                                    <MyMap
+                                        gigsList={this.state.gigsList}
+                                        mapVisible={() => this.mapVisible()}
+                                    />
+                                )}
+                            />
+                            <Route
+                                exact
+                                path="/gig-list"
+                                render={(props) => (
+                                    <GigList
+                                        gigsList={this.state.gigsList}
+                                        listSet={(e) => this.listSet(e)}
+                                    />
+                                )}
+                            />
+                            <Route
+                                path="/chat"
+                                render={(props) => (
+                                    <Chat
+                                        chat_img={this.state.chat_img}
+                                        chat_myUserId={this.state.id}
+                                        chat_color={this.state.chat_color}
+                                    />
+                                )}
+                            />
                         </div>
-                        <a target="_blank" href="https://www.1000mods.com">
-                            <div className="logo2Back">
-                                <div className="logo2"></div>
-                            </div>
-                        </a>
-                        <div className="logout" onClick={() => this.logOut()}>
-                            LogOut
-                        </div>
-                        {this.state.maps && (
-                            <Link
-                                to="/"
-                                className="barMainLink"
-                                onClick={() => this.mapVisible()}
-                            ></Link>
-                        )}
                     </div>
-                    <Route
-                        exact
-                        path="/"
-                        render={(props) => (
-                            <Main
-                                admin={this.state.admin}
-                                listSet={(e) => this.listSet(e)}
-                                visitors={this.state.visitors}
-                            />
-                        )}
-                    />
-                    <Route
-                        exact
-                        path="/gig-creator"
-                        render={(props) => (
-                            <GigCreator admin={this.state.admin} />
-                        )}
-                    />
-                    <Route
-                        exact
-                        path="/gig-editor"
-                        render={(props) => (
-                            <GigEditor gigsList={this.state.gigsList} />
-                        )}
-                    />
-                    <Route
-                        exact
-                        path="/map"
-                        render={(props) => (
-                            <MyMap
-                                gigsList={this.state.gigsList}
-                                mapVisible={() => this.mapVisible()}
-                            />
-                        )}
-                    />
-                    <Route
-                        exact
-                        path="/gig-list"
-                        render={(props) => (
-                            <GigList
-                                gigsList={this.state.gigsList}
-                                listSet={(e) => this.listSet(e)}
-                            />
-                        )}
-                    />
-                    <Route
-                        path="/chat"
-                        render={(props) => (
-                            <Chat
-                                chat_img={this.state.chat_img}
-                                chat_myUserId={this.state.id}
-                                chat_color={this.state.chat_color}
-                            />
-                        )}
-                    />
+                    <div
+                        className="introCover"
+                        style={{
+                            width: this.state.left + "px",
+                        }}
+                    >
+                        <div
+                            className="intro"
+                            style={{
+                                width: this.state.left + "px",
+                            }}
+                        >
+                            Welcome
+                            <span>
+                                Hover on the right to find the "slider" and then
+                                move it horizontally to see behind the void.
+                            </span>
+                        </div>
+                    </div>
+                    <div
+                        style={{
+                            left:
+                                this.state.left - this.state.sliderWidth + "px",
+                        }}
+                        className="slider"
+                        onMouseDown={(e) => this.sliderAction(e)}
+                    ></div>
                 </div>
             </BrowserRouter>
         );
