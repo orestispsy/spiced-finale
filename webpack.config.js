35e0eed318a68aa06d2de0f4af7b9d4fb4de5f35
Orestis Psycharis <opsychar@gmail.com>
2021-08-05 10:11:36 +0200

    chat design fixes

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,7 @@ module.exports = () => ({
         path.join(__dirname, "client", "style.css"),
         path.join(__dirname, "client", "src", "start.js"),
     ],
-  
-  
+
     output: {
         path: path.join(__dirname, "client", "public"),
         filename: "bundle.js",
